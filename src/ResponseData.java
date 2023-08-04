435fbad8e1441b88726ef379465f1889cf7c2de4
Nail Sharipov <sharipovn@gmail.com>
2023-08-04 19:12:23 +0500

    создаю netty сервер и клиент по статье https://www.baeldung.com/netty

---
+++ src/ResponseData.java
@@ -0,0 +1,13 @@
+public class ResponseData {
+    private int intValue;
+
+    // standard getters and setters
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
+}
